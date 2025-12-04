e01f65825b654c3a1321a3226ce766807e20b390
kozae00 <rhwogus777@gmail.com>
2025-12-04 20:30:27 +0900

    init

---
+++ src/test/java/com/example/planitsquarebeproject/PlanitsquareBeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.planitsquarebeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanitsquareBeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
