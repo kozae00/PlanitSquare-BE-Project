bad6609233487c61c7ce87e08cf326d9a5f8e345
kozae00 <rhwogus777@gmail.com>
2025-12-05 01:18:27 +0900

    feat: NagerApiClient

--- src/main/java/com/example/planitsquarebeproject/global/infrastructure/NagerApiClient.java
+++ src/main/java/com/example/planitsquarebeproject/global/infrastructure/NagerApiClient.java
@@ -1,4 +1,27 @@
 package com.example.planitsquarebeproject.global.infrastructure;
 
+import com.example.planitsquarebeproject.domain.country.dto.CountryDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
 public class NagerApiClient {
+
+    private final WebClient webClient = WebClient.builder()
+            .baseUrl("https://date.nager.at/api/v3")
+            .build();
+
+    public List<CountryDto.Response> getAvailableCountries() {
+        return webClient.get()
+                .uri("/AvailableCountries")
+                .retrieve()
+                .bodyToFlux(CountryDto.Response.class)
+                .collectList()
+                .block();
+    }
 }
